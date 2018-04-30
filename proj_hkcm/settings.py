4f2a0528cb5899c2c23f5668d7f5019d8696ec24
PharrellWANG <wzxnuaa@gmail.com>
2018-04-30 23:41:11 +0800

    optimizations

--- proj_hkcm/settings.py
+++ proj_hkcm/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from proj_hkcm.dev_db import DEV_DATABASES
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,6 +74,7 @@ WSGI_APPLICATION = 'proj_hkcm.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if DEBUG:
+    from proj_hkcm.dev_db import DEV_DATABASES
     DATABASES = DEV_DATABASES
 else:
     DATABASES = {
